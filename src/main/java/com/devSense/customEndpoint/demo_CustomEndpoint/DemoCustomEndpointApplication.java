4292369958a0552cf086e0a815105285864bfec8
coolmp08 <coolmp08@gmail.com>
2024-12-11 11:56:21 +0530

    Added my project

---
+++ src/main/java/com/devSense/customEndpoint/demo_CustomEndpoint/DemoCustomEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.devSense.customEndpoint.demo_CustomEndpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCustomEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCustomEndpointApplication.class, args);
+	}
+
+}
